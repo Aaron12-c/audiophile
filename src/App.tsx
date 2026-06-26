f1a3dc7226258a0839f39923bc4f148d6c4112f2
Aaron Hope <aaronhope961@gmail.com>
2026-06-26 05:33:36 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './Pages/HomePage';
+import HeadphonePage from './Pages/HeadphonePage'; // Import this
+// Import these when you create them
+// import SpeakersPage from './Pages/SpeakersPage';
+// import EarphonesPage from './Pages/EarphonesPage';
 
 function App() {
   return (
@@ -10,11 +13,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-            
           <Route path="/" element={<HomePage />} />
-          <Route path="/headphones" element={<HomePage />} />
-          <Route path="/speakers" element={<HomePage />} />
-          <Route path="/earphones" element={<HomePage />} />
+          <Route path="/headphones" element={<HeadphonePage />} /> {/* Changed from HomePage to HeadphonePage */}
+          <Route path="/speakers" element={<HomePage />} /> {/* Change to SpeakersPage when ready */}
+          <Route path="/earphones" element={<HomePage />} /> {/* Change to EarphonesPage when ready */}
         </Routes>
         <Footer />
       </div>
